54195a06749c2a8511ddc73492d973491589f758
Dawid Barsznica <dbarsznica@gmail.com>
2019-02-21 13:08:29 +0100

    Update JS

--- js/script.js
+++ js/script.js
@@ -1,38 +1,32 @@
 var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
+var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true
 });
 
 // element argument can be a selector string
 //   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
+var flkty = new Flickity('.main-carousel', {
+    hash: true,
 });
 
-var flkty = new Flickity( '.main-carousel', {
-    hash: true,
-  });
 
-  var flkty = new Flickity('.main-carousel');
+var progressBar = document.querySelector('.progress-bar')
 
-  var progressBar = document.querySelector('.progress-bar')
-  
-  flkty.on( 'scroll', function( progress ) {
-    progress = Math.max( 0, Math.min( 1, progress ) );
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
     progressBar.style.width = progress * 100 + '%';
-  });
+});
 
-  var flkty = new Flickity('.main-carousel');
 
 var buttonGroup = document.querySelector('.button-group');
 
-buttonGroup.addEventListener( 'click', function( event ) {
-  // filter for button clicks
-  if ( !matchesSelector( event.target, '.button' ) ) {
-    return;
-  }
-  var selector = event.target.getAttribute('data-selector');
-  flkty.selectCell( selector );
+buttonGroup.addEventListener('click', function (event) {
+    // filter for button clicks
+    if (!matchesSelector(event.target, '.button')) {
+        return;
+    }
+    var selector = event.target.getAttribute('data-selector');
+    flkty.selectCell(selector);
 });
