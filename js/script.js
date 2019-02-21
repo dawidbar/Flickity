43904e2a051822b7647640f5499345a270ae2607
Dawid Barsznica <dbarsznica@gmail.com>
2019-02-21 19:53:18 +0100

    Update carousel

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 'use strict';
-(function(){ 
+(function () {
 
-var templateItem = document.getElementById('template-product-item').innerHTML;
-Mustache.parse(templateItem);
-var listItems = '';
-for(var i = 0; i < items.length; i++){
-    console.log(items);
-    listItems += Mustache.render(templateItem, items[i]);
-}
-var main = document.querySelector('.main-carousel');
-main.innerHTML = listItems;
-})(); 
+    var templateItem = document.getElementById('template-product-item').innerHTML;
+    Mustache.parse(templateItem);
+    var listItems = '';
+    for (var i = 0; i < items.length; i++) {
+        console.log(items);
+        listItems += Mustache.render(templateItem, items[i]);
+    }
+    var main = document.querySelector('.main-carousel');
+    main.innerHTML = listItems;
+})();
 
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity(elem, {
