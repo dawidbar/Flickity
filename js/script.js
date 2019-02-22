595550486fe5ca2cbea677c4379f53265dd064a8
Dawid Barsznica <dbarsznica@gmail.com>
2019-02-22 20:48:50 +0100

    Fix bugs

--- js/script.js
+++ js/script.js
@@ -11,30 +11,30 @@
     main.innerHTML = listItems;
 
 
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity(elem, {
-    // options
-    cellAlign: 'left',
-    contain: true,
-    hash: true
-});
+    var elem = document.querySelector('.main-carousel');
+    var flkty = new Flickity(elem, {
+        // options
+        cellAlign: 'left',
+        contain: true,
+        hash: true
+    });
 
-var progressBar = document.querySelector('.progress-bar')
+    var progressBar = document.querySelector('.progress-bar')
 
-flkty.on('scroll', function (progress) {
-    progress = Math.max(0, Math.min(1, progress));
-    progressBar.style.width = progress * 100 + '%';
-});
+    flkty.on('scroll', function (progress) {
+        progress = Math.max(0, Math.min(1, progress));
+        progressBar.style.width = progress * 100 + '%';
+    });
 
 
-var button = document.querySelector('.button');
+    var button = document.querySelector('.button');
 
-button.addEventListener('click', function (event) {
-    // filter for button clicks
-    if (!matchesSelector(event.target, '.button')) {
-        return;
-    }
-    var selector = event.target.getAttribute('data-selector');
-    flkty.selectCell(selector);
-});
+    button.addEventListener('click', function (event) {
+        // filter for button clicks
+        if (!matchesSelector(event.target, '.button')) {
+            return;
+        }
+        var selector = event.target.getAttribute('data-selector');
+        flkty.selectCell(selector);
+    });
 })();
